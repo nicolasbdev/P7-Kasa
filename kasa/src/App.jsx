090eefa93c0584217d42e93d9e27c3b5766a3fe5
nicolasbdev <nicolas.behaegel1@gmail.com>
2025-08-06 14:30:59 +0200

    Routing réalisé dans App.jsx

--- kasa/src/App.jsx
+++ kasa/src/App.jsx
@@ -1,1 +1,26 @@
+import Header from "./Components/Header"
+import Footer from "./Components/Footer"
+import About from "./Pages/About"
+import Home from "./Pages/Home"
+import Logement from "./Pages/Logement"
+import Error from "./Pages/Error"
+import {BrowserRouter, Routes, Route} from 'react-router-dom'
 
+
+const App = () =>{
+	return (
+		<BrowserRouter>
+		<Header/>
+		<Routes>
+			<Route path="/" element={<Home/>}/>
+			<Route path="/about" element={<About/>}/>
+			<Route path="logements/:id" element={<Logement/>}/>
+			<Route path="*" element={<Error/>}/>
+
+
+		
+		</Routes>
+		<Footer/>
+		</BrowserRouter>
+	);
+}
