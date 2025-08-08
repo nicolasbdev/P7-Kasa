98bdb96edb12ac10ece7a80fa927a9424d3e2ecf
NicolasB <nicolas.behaegel1@gmail.comL>
2025-08-08 10:56:27 +0200

    Import des images et réalisation du footer

---
+++ kasa/src/Components/Footer.jsx
@@ -0,0 +1,14 @@
+import logo from "../assets/Logo/logofooter.png";
+
+function Footer(){
+    return(
+        <footer>
+            <div className="footer">
+                <img className="logo-footer" src={logo} alt="Logo de l'entreprise"/>
+                <p>@2025 kasa. All right reserved</p>
+            </div>
+        </footer>
+    );
+}
+
+export default Footer;
