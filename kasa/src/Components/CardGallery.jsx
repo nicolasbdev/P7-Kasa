027a851676f4d714665fb239cd5363b2b2690d34
NicolasB <nicolas.behaegel1@gmail.comL>
2025-10-20 10:43:02 +0200

    CardGallery JSX et gallery SCSS modifiées pour ajouter le dégradé

--- kasa/src/Components/CardGallery.jsx
+++ kasa/src/Components/CardGallery.jsx
@@ -6,9 +6,10 @@ const CardGallery = ()=> {
     <div className="gallery">
         {/* Extraction des données du fichier JSON avec la méthode Map. Cela permet de récupérer les données pour les cards de la page Home. */}      
         {Logements.map((logement) => (
-              <Link to={`/logements/${logement.id}`} className='gallery-item' key={logement.id}>  
-                <img src={logement.cover} alt={logement.title} />
-                <h3>{logement.title}</h3>
+              <Link to={`/logements/${logement.id}`} className='gallery-item' key={logement.id}>
+                  <img src={logement.cover} alt={logement.title} />
+                  <div className="gradient-filter"></div>
+                  <h3>{logement.title}</h3>
               </Link>
             ))}
         {/* Ci-dessus création du lien dirige le clic sur une card vers une page Logement correspondante */}
